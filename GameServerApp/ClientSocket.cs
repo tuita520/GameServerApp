341005118b5a0e9734569ed13e90f1fd5a32a806
Alex <proud_bm@163.com>
2018-05-10 17:21:18 +0800

    2018.5.10

--- GameServerApp/ClientSocket.cs
+++ GameServerApp/ClientSocket.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Threading;
 
@@ -9,6 +10,27 @@ namespace GameServerApp
     /// </summary>
     public class ClientSocket
     {
+        #region 单例
+        private static ClientSocket instance;
+
+        public static ClientSocket Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ClientSocket();
+                }
+                return instance;
+            }
+        }
+
+        private ClientSocket()
+        {
+
+        }
+        #endregion
+
         #region 参数
         /// <summary>
         /// 客户端的Socket
@@ -21,6 +43,13 @@ namespace GameServerApp
         private Thread m_ReceiveThread;
 
         /// <summary>
+        /// 所属角色
+        /// </summary>
+        private Role m_Role;
+        #endregion
+
+        #region 消息接收参数
+        /// <summary>
         /// 客户端数据的缓存区域
         /// </summary>
         private byte[] m_ReceiveBuffer = new byte[10240];
@@ -29,13 +58,21 @@ namespace GameServerApp
         /// 客户端数据的读写操作
         /// </summary>
         private MMOMemoryStream m_ReceiveMS = new MMOMemoryStream();
-
-        /// <summary>
-        /// 所属角色
-        /// </summary>
-        private Role m_Role;
         #endregion
 
+        #region 消息发送参数
+        /// <summary>
+        /// 消息队列
+        /// </summary>
+        private Queue<byte[]> m_MSgQueue = new Queue<byte[]>();
+
+        /// <summary>
+        /// 队列检查委托
+        /// </summary>
+        private Action m_CheckQueue;
+        #endregion
+
+        #region ClientSocket 构造函数（初始化）
         /// <summary>
         /// 构造函数（初始化）
         /// </summary>
@@ -51,8 +88,23 @@ namespace GameServerApp
             m_ReceiveThread = new Thread(ReceiveMessage);
             //开启
             m_ReceiveThread.Start();
-        }
 
+            //委托绑定
+            m_CheckQueue = CheckQueue;
+
+            //测试
+            using (MMOMemoryStream ms = new MMOMemoryStream())
+            {
+                ms.WriterUTF8String(string.Format("当前连接时间" + DateTime.Now.ToString()));
+
+                SendMsg(ms.ToArray());
+            }
+        }
+        #endregion
+
+        //==========异步接收数据==========
+
+        #region ReceiveMessage 接收数据处理
         /// <summary>
         /// 接收数据处理
         /// </summary>
@@ -61,7 +113,9 @@ namespace GameServerApp
             //开始接收（异步）
             m_Socket.BeginReceive(m_ReceiveBuffer, 0, m_ReceiveBuffer.Length, SocketFlags.None, ReceiveCallBack, m_Socket);
         }
+        #endregion
 
+        #region ReceiveCallBack 异步数据接收的回调（实现循环）
         /// <summary>
         /// 异步数据接收的回调（实现循环）
         /// </summary>
@@ -120,6 +174,7 @@ namespace GameServerApp
                                 //剩余字节的长度
                                 int remainLen = (int)m_ReceiveMS.Length - curAllMsgLen;
 
+                                //有剩余的字节长度
                                 if (remainLen > 0)
                                 {
                                     //设定在上一个数据包的尾部
@@ -138,16 +193,29 @@ namespace GameServerApp
                                     //重新写入数据流
                                     m_ReceiveMS.Write(remainBuffer, 0, remainLen);
 
-                                    //重新定位，在当前数据的最后位置，下次客户端传输的数据跟在该数据后
-                                    m_ReceiveMS.Position = m_ReceiveMS.Length;
+                                    //剩余空间不需要使用，应当释放
+                                    remainBuffer = null;
                                 }
+                                //无剩余的字节长度
                                 else
                                 {
+                                    //清空数据流
+                                    m_ReceiveMS.Position = 0;
+                                    m_ReceiveMS.SetLength(0);
 
+                                    break;
                                 }
+                            }
+                            //数据不完整，不拆包，等待下次数据写入
+                            else
+                            {
+                                break;
                             }
                         }
                     }
+
+                    //进行下一次数据包的接收
+                    ReceiveMessage();
                 }
                 else
                 {
@@ -168,5 +236,100 @@ namespace GameServerApp
                 Console.WriteLine("当前角色集合的长度为：{0}", RoleManager.Instance.AllRole.Count);
             }
         }
+        #endregion
+
+        //==========异步发送数据==========
+
+        #region SendMsg 得到完整数据存入队列
+        /// <summary>
+        /// 消息发送（得到包体部分）
+        /// </summary>
+        /// <param name="_message"></param>
+        public void SendMsg(byte[] _message)
+        {
+            //制作完整数据包
+            byte[] sendBuffer = MakeAllData(_message);
+
+            lock (m_MSgQueue)
+            {
+                //放置在Queue队列中
+                m_MSgQueue.Enqueue(sendBuffer);
+
+                //开启委托（委托实现）
+                m_CheckQueue.BeginInvoke(null, null);
+            }
+        }
+        #endregion
+
+        #region MakeAllData 制作完整数据包（包头+包体）
+        /// <summary>
+        /// 制作完整的数据包（包头+包体）
+        /// </summary>
+        /// <param name="_data"></param>
+        /// <returns></returns>
+        private byte[] MakeAllData(byte[] _data)
+        {
+            byte[] retBuffer = null;
+
+            using (MMOMemoryStream ms = new MMOMemoryStream())
+            {
+                //ushort占两个字节，作为包头的长度
+                //将长度转换成byte，写入数据流中
+                ms.WriteUshort((ushort)_data.Length);
+                //将数据写入数据流
+                ms.Write(_data, 0, _data.Length);
+
+                //读取
+                retBuffer = ms.ToArray();
+            }
+
+            return retBuffer;
+        }
+        #endregion
+
+        #region CheckQueue 根据队列是否有值进行数据发送
+        /// <summary>
+        /// 检查队列的委托回调函数
+        /// </summary>
+        private void CheckQueue()
+        {
+            lock (m_MSgQueue)
+            {
+                //队列中至少有一条数据
+                if (m_MSgQueue.Count > 0)
+                {
+                    //发送数据
+                    Send(m_MSgQueue.Dequeue());
+                }
+            }
+        }
+        #endregion
+
+        #region Send 用于将数据包发送到服务器
+        /// <summary>
+        /// 用于将数据包发送到服务器
+        /// </summary>
+        /// <param name="buffer"></param>
+        private void Send(byte[] buffer)
+        {
+            //异步发送
+            m_Socket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, SendCallBack, m_Socket);
+        }
+        #endregion
+
+        #region SendCallBack 异步发送消息的回调函数
+        /// <summary>
+        /// 异步发送消息的回调函数
+        /// </summary>
+        /// <param name="ar"></param>
+        private void SendCallBack(IAsyncResult ar)
+        {
+            //完成异步
+            m_Socket.EndSend(ar);
+
+            //检查队列
+            CheckQueue();
+        }
+        #endregion
     }
 }
